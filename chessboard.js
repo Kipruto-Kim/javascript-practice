3c2b82c643a50a64f7d1ff82bcaed01e609adc69
Bruce <brucekip27@gmail.com>
2025-01-27 18:48:47 +0300

    here

---
+++ chessboard.js
@@ -0,0 +1,13 @@
+// console.log("# # # # \n # # # #\n# # # # \n # # # #\n# # # # \n # # # #\n# # # # \n # # # # ")
+
+let chessboard = ' '
+for ( let x = 0; x < 8; x++){
+    for ( let y = 0; y < 8; y++){
+        if (( x + y ) % 2 === 0){
+            chessboard += ' '
+        }else{
+            chessboard += '#'
+        }
+    }chessboard += '\n'
+}
+console.log(chessboard)
