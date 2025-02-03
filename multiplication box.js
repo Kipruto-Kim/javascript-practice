050fded1f1e333448fa84093eff18ea8b51aec03
Bruce <brucekip27@gmail.com>
2025-02-03 20:28:50 +0300

    commit

--- multiplication box.js
+++ multiplication box.js
@@ -1,6 +1,6 @@
-for ( let x = 1; x <= 10; x++){
+for ( let x = 1; x <= 20; x++){
     let MBox = ""
-    for ( let y = 1; y <= 10; y++){
+    for ( let y = 1; y <= 20; y++){
         MBox += ( x * y) + "\t"
     }console.log(MBox)
 }
